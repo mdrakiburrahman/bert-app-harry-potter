b9b0451e2bfe907556c9c38f90d82f1022b4f4c5
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2020-02-12 21:12:49 -0500

    Initial commit

---
+++ 2.docker-deploy/cdQA-ui-test-app/README.md
@@ -0,0 +1,98 @@
+# cdqa-ui
+
+[![Build Status](https://travis-ci.com/cdqa-suite/cdQA-ui.svg?token=Vzy9RRKRZ41ynd9q2BRX&branch=master)](https://travis-ci.com/cdqa-suite/cdQA-ui)
+[![NPM Version](https://img.shields.io/npm/v/cdqa-ui.svg)](https://www.npmjs.com/package/cdqa-ui)
+[![NPM Downloads](https://img.shields.io/npm/dm/cdqa-ui.svg)](https://www.npmjs.com/package/cdqa-ui)
+![GitHub](https://img.shields.io/github/license/cdqa-suite/cdQA-ui.svg)
+
+A user interface for cdQA
+
+![](https://cdqa-suite.github.io/cdQA-website/img/suite-2.822c5624.png)
+
+## Table of contents <!-- omit in toc -->
+
+- [Installation](#Installation)
+- [Import](#Import)
+- [Usage](#Usage)
+- [Demo](#Demo)
+- [Properties](#Properties)
+- [Development](#Development)
+- [License](#License)
+
+## Installation
+
+```shell
+npm install cdqa-ui
+```
+
+## Import
+
+```javascript
+import Vue from 'vue'
+import CdqaUI from 'cdqa-ui'
+
+Vue.use(CdqaUI)
+```
+
+`cdqa-ui` relies on [`bootstrap-vue`](https://bootstrap-vue.js.org/), an external dependency that is not bundled with the component. Therefore you also need to import `bootstrap-vue` in your app entrypoint:
+
+```javascript
+import Vue from 'vue'
+import BootstrapVue from "bootstrap-vue"
+
+Vue.use(BootstrapVue)
+
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+```
+
+## Usage
+
+```xml
+<CdqaUI api_endpoint_cpu="http://localhost:5000/api" :queries_examples="['What is Artificial Intelligence?', 'What is Blockchain?']"></CdqaUI>
+```
+
+## Demo
+
+Check our demo live at: https://cdqa-suite.github.io/cdQA-website
+
+## Properties
+
+| Name         | Type   | Default                     | Description                      |
+| ------------ | ------ | --------------------------- | -------------------------------- |
+| api_endpoint_cpu | string | `http://localhost:5000/api` | The url of your `cdQA` REST API on CPU. |
+| api_endpoint_gpu | string | `http://localhost:5000/api` | The url of your `cdQA` REST API on GPU. |
+| queries_examples | array | `['What is artificial intelligence?', 'What is natural language processing?']` | The list of example queries. |
+
+## Development
+
+Clone the repository
+
+```shell
+git clone https://github.com/cdqa-suite/cdQA-ui
+cd cdQA-ui
+```
+
+Install package
+
+```shell
+npm install
+```
+
+Compiles and hot-reloads for development
+
+```shell
+npm run serve
+```
+
+Compiles and minifies for production
+
+```shell
+npm run build
+```
+
+This repo is based on [vue-sfc-rollup](https://github.com/team-innovation/vue-sfc-rollup), a minimal setup to produce npm-ready vue components. Releases are done automatically on master pushes (see [travis.yml](travis.yml)).
+
+## License
+
+Apache-2.0
---
+++ 2.docker-deploy/cdQA-ui-test-app/src/cdqaUI.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="cdqaUI">
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+      integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
+      crossorigin="anonymous"
+    >
+    <div class="input m-4">
+      <toggle-button
+        class="mb-3"
+        v-model="gpu"
+        :value="false"
+        color="#AE41A7"
+        :sync="true"
+        :width="55"
+        :labels="{checked: 'GPU', unchecked: 'CPU'}"
+      />
+      <b-form @submit="onSubmit">
+        <b-input-group>
+          <b-dropdown slot="prepend" text="Choose an example..." variant="outline-secondary" size="sm">
+            <b-dropdown-item v-for="i in queries_examples" :key="i" @click="query = i">{{ i }}</b-dropdown-item>
+          </b-dropdown>
+          <b-form-input v-model="query" placeholder="or type a question..."></b-form-input>
+          <b-input-group-append>
+            <b-button class="gradient-fill background hover" @click="onSubmit">
+              <b-spinner v-if="status == 'loading'" small :variant="'white'" label="Small Spinner"></b-spinner>
+              <i v-else class="fa fa-search"></i>
+            </b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </b-form>
+      <br>
+
+      <div v-if="status == 'done' && query != ''">
+        <div class="mb-4">
+          <span class="gradient-fill">Answer</span>
+          <br>
+          <span>{{ answer }}</span>
+        </div>
+        <div class="mb-4">
+          <span class="gradient-fill">Passage Context</span>
+          <br>
+          <Highlighter
+            highlightClassName="gradient-fill background hover"
+            :searchWords="[answer]"
+            :autoEscape="true"
+            :textToHighlight="paragraph"
+          />
+        </div>
+        <span class="gradient-fill">Original Document</span>
+        <br>
+        <span>{{ title }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Highlighter from "vue-highlight-words";
+import { ToggleButton } from 'vue-js-toggle-button'
+
+export default {
+  name: "CdqaUI",
+  components: {
+    Highlighter,
+    ToggleButton
+  },
+  props: {
+    api_endpoint_cpu: {
+      type: String,
+      default: "http://cdQAUIappdemo.canadacentral.azurecontainer.io:5000/api"
+    },
+    api_endpoint_gpu: {
+      type: String,
+      default: "http://cdQAUIappdemo.canadacentral.azurecontainer.io:5000/api"
+    },
+    queries_examples: {
+      type: Array,
+      default: function () {
+        return ['What is artificial intelligence?', 'What is natural language processing?']
+      }
+    },
+  },
+  data() {
+    return {
+      query: "",
+      show: false,
+      status: "started",
+      answer: "",
+      title: "",
+      paragraph: "",
+      gpu: false
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.status = "loading";
+      if (this.gpu) {
+        var api_endpoint = this.api_endpoint_gpu
+      } else {
+        api_endpoint = this.api_endpoint_cpu
+      }
+      let self = this;
+      axios
+        .get(api_endpoint, { params: { query: self.query } })
+        .then(function(response) {
+          self.answer = response.data.answer;
+          self.title = response.data.title;
+          self.paragraph = response.data.paragraph;
+          self.status = "done";
+        })
+        .catch(function(error) {
+          alert(error);
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.cdqaUI {
+  text-align: left;
+  font-family: Inter, Inter UI, Inter-UI, SF Pro Display, SF UI Text,
+    Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-weight: 400;
+  letter-spacing: +0.37px;
+  color: rgb(175, 175, 175);
+}
+
+.form-control:focus {
+  border-color: #ae41a7 !important;
+  box-shadow: 0 0 5px #ae41a7 !important;
+}
+
+.gradient-fill {
+  background-image: linear-gradient(
+    -225deg,
+    #a445b2 0%,
+    #d41872 52%,
+    #ff0066 100%
+  );
+}
+
+.gradient-fill.background {
+  background-size: 250% auto;
+  border: medium none currentcolor;
+  border-image: none 100% 1 0 stretch;
+  transition-delay: 0s, 0s, 0s, 0s, 0s, 0s;
+  transition-duration: 0.5s, 0.2s, 0.2s, 0.2s, 0.2s, 0.2s;
+  transition-property: background-position, transform, box-shadow, border,
+    transform, box-shadow;
+  transition-timing-function: ease-out, ease, ease, ease, ease, ease;
+  color: white;
+  font-weight: bold;
+  border-radius: 3px;
+}
+
+span.gradient-fill {
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 2.5;
+}
+</style>
